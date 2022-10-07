c04354ff4b40ba56df40c2142ff9a8c5d555e0fb
Cristhian R. Guerrero Soto <cristian.guerrero@guayas.gob.ec>
2022-10-07 08:33:52 -0500

    Scaffolding Vite + AntDesign + React TS del SGP+ | Hago adaptable HEADER según el dispositivo.

--- src/ui/Header/HeaderContent.tsx
+++ src/ui/Header/HeaderContent.tsx
@@ -1,32 +1,30 @@
+import { useLocation } from 'react-router-dom';
 import { Breadcrumb, Col, Row, Typography } from 'antd';
 
 export const HeaderContent = () => {
+    const { pathname } = useLocation();
+    const paths = pathname.split('/').filter(i => i);
+
     return (
         <>
             <Row style={localStyles.row}>
-                <Col span={24}>
+                <Col
+                    flex={2}
+                >
                     <Typography.Title level={3}>Titulo</Typography.Title>
                 </Col>
-            </Row>
-            <Row style={localStyles.row}>
-                <Col span={12}>
-                    <Typography.Text type='secondary'>Info</Typography.Text>
-                </Col>
                 <Col
-                    span={12}
+                    flex={3}
                     style={localStyles.breadcrumb}
                 >
                     <Breadcrumb>
-                        <Breadcrumb.Item>
-                            Inicia
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item>
-                            Medio
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item>
-                            Final
-                        </Breadcrumb.Item>
+                        {paths.map((value) => <Breadcrumb.Item>{value}</Breadcrumb.Item>)}
                     </Breadcrumb>
+                </Col>
+            </Row>
+            <Row style={localStyles.row}>
+                <Col>
+                    <Typography.Text type='secondary'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ducimus impedit placeat sunt nesciunt molestias illo delectus asperiores, enim facilis ex ipsam dolore ad doloremque, quia tempora distinctio mollitia temporibus!</Typography.Text>
                 </Col>
             </Row>
         </>
